143585d307072b4b9bde4fc1978710b1c0d3efb2
coder2j <coder2j@MacBook-Pro-X.fritz.box>
2022-02-19 14:35:13 +0100

    MINOR: Add Tutorial of Airflow catchup and backfill

---
+++ dags/dag_with_catchup_and_backfill.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    'owner': 'coder2j',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='dag_with_catchup_backfill_v02',
+    default_args=default_args,
+    start_date=datetime(2021, 11, 1),
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+    task1 = BashOperator(
+        task_id='task1',
+        bash_command='echo This is a simple bash command!'
+    )
